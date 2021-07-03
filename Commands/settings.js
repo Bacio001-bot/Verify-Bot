bffec57295e7f915231caaf4712efbc5fae7bb57
ps209658 <81424361+ps209658@users.noreply.github.com>
2021-07-03 19:37:03 +0200

    Added perm check

--- Commands/settings.js
+++ Commands/settings.js
@@ -4,8 +4,11 @@ module.exports = {
     name: 'settings',
     async execute(bot, message, PREFIX, Discord, settings, args) {
 
+        if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(`You do not have the right permissions`)
+
         if (args[1].toLowerCase() == `verifytoggle` && args[2].toLowerCase() == `on` || args[2].toLowerCase() == `enable`) {
 
+            //Update status to true where guild id is id
             await Guild.findOneAndUpdate({
                 guildid: message.guild.id
             }, {
@@ -18,6 +21,7 @@ module.exports = {
 
         if (args[1].toLowerCase() == `verifytoggle` && args[2].toLowerCase() == `off` || args[2].toLowerCase() == `disable`) {
 
+            //Update status to false where guild id is id
             await Guild.findOneAndUpdate({
                 guildid: message.guild.id
             }, {
@@ -33,7 +37,8 @@ module.exports = {
             let role = message.guild.roles.cache.find(dbrole => dbrole.id === args[2])
 
             if (!role) return message.channel.send(`You didn't supply a valid role`)
-
+            
+            //Update role to role id where guild id is id
             await Guild.findOneAndUpdate({
                 guildid: message.guild.id
             }, {
